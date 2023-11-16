009a9ea56948afa8126dd9cd7e79d6871245f631
colin <colin.williams.dev@gmail.com>
2023-11-16 13:19:26 -0600

    feat: ctor and pvt field dependency injection interface for loose coupling

--- DbMigrator.cs
+++ DbMigrator.cs
@@ -4,6 +4,14 @@ namespace csharp_intermediate_interfaces_dependency_injection
 {
     public class DbMigrator
     {
+        private readonly ILogger _logger;
+
+        /* Dependency Injection: pass into ctor and assign to private field */
+        public DbMigrator(ILogger logger)
+        {
+            this._logger = logger;
+        }
+
         public void Migrate()
         {
             Console.WriteLine($"Migration started at {DateTime.Now}");
