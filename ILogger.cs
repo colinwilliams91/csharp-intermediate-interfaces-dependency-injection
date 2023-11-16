17164505f9f24fcc6f190d8ab0b21a152b31a0d4
colin <colin.williams.dev@gmail.com>
2023-11-16 13:16:29 -0600

    feat: init ILogger interface exposing 2 Log methods

---
+++ ILogger.cs
@@ -0,0 +1,8 @@
+﻿namespace csharp_intermediate_interfaces_dependency_injection
+{
+    public interface ILogger
+    {
+        void LogError(string message);
+        void LogInfo(string message);
+    }
+}
