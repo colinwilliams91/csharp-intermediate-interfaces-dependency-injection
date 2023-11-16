5d330f22374c8069306e925a272b46513aae27cf
colin <colin.williams.dev@gmail.com>
2023-11-16 13:26:42 -0600

    feat: init ConsoleLogger concrete class implements ILogger Interface

---
+++ ConsoleLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace csharp_intermediate_interfaces_dependency_injection
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void LogError(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogInfo(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
