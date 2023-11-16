b91d752dc0f113a2c0235dcf01dd7af2e54b76a8
colin <colin.williams.dev@gmail.com>
2023-11-16 13:14:55 -0600

    feat: extract DbMigrator class from Program, add public Migrate method

---
+++ DbMigrator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace csharp_intermediate_interfaces_dependency_injection
+{
+    public class DbMigrator
+    {
+        public void Migrate()
+        {
+            Console.WriteLine($"Migration started at {DateTime.Now}");
+
+            // Details of migrating the database
+
+            Console.WriteLine($"Migration finished at {DateTime.Now}");
+        }
+    }
+}
