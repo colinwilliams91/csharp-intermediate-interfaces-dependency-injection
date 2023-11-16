49c75dd963d3efff9c76755c6df875f2d01b7ff8
colin <colin.williams.dev@gmail.com>
2023-11-16 13:58:45 -0600

    feat: init FileLogger w IO StreamWriter to log to file, implements ILogger

---
+++ FileLogger.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace csharp_intermediate_interfaces_dependency_injection
+{
+    public class FileLogger : ILogger
+    {
+        private readonly string _path;
+
+        public FileLogger(string path)
+        {
+            this._path = path;
+        }
+
+        public void LogError(string message)
+        {
+            /* StreamWriter comes from System.IO and will create a new file if not already exists (2nd arg: true == append, false == overwrite) */
+            var streamWriter = new StreamWriter(this._path, true);
+        }
+
+        public void LogInfo(string message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
