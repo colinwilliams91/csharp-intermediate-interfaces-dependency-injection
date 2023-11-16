9903382561c01bcf69d44ef1d75ce5c8c70ed391
colin <colin.williams.dev@gmail.com>
2023-11-16 13:08:02 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_intermediate_interfaces_dependency_injection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
